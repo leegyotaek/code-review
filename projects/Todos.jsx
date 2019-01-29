13812a6fe99971942a77a5a6baca45f05cf9a76b
이교택 <leegyotaek@gmail.com>
2019-01-29 16:06:55 +0900

    My first Commit

---
+++ projects/Todos.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Todos = (props) => {
+
+  console.log('---->', props)
+
+  return (
+    <div className="App">
+      {/*<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>*/}
+      <h2>{props.title}</h2>
+      <ul>
+        {props.items.map((item, index)=> {
+          return <li key={'todo'+index}> <input type ="checkbox"/>{item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos
